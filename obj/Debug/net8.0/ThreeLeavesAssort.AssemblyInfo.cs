d1b0297e2232e35ae619f412882a67a4bbc45b89
Yuto5959 <yuto.misora5959@gmail.com>
2025-03-05 15:29:36 +0900

    Image Expand Edit

--- obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+348239d398b9c799f76b5aba823fe714d7ff470b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+214af3ea47768873994cdf9512c286df6b6693dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
