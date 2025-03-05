f9434577e992c25d03037af26364de0cad0ee442
Yuto5959 <yuto.misora5959@gmail.com>
2025-03-05 20:09:03 +0900

    Created Top Page on Effect

--- obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c46fd684734842819b43697fc20948c2493b014a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c932bb816d9453a06e82c7e7317f983e9bebe0c")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
