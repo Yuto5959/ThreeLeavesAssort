b69c19d7fb2f3e88bc7cda015b4f788d11b5ea95
Yuto5959 <yuto.misora5959@gmail.com>
2025-03-05 16:37:31 +0900

    Fix ImageFile

--- obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
+++ obj/Debug/net8.0/ThreeLeavesAssort.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1b0297e2232e35ae619f412882a67a4bbc45b89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6d069d031798ad200bb857fc66b67d443207403")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThreeLeavesAssort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
